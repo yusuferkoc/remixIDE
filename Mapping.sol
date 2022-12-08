a28fc82814d4ff4874b05a26c5d531ea6574ab68
yusuf <yusuferkoc99@gmail.com>
2022-12-08 16:44:45 +0300

    maping

--- Mapping.sol
+++ Mapping.sol
@@ -23,3 +23,20 @@ contract Mapping{
         delete(favNums[msg.sender]);
     }
 }
+
+contract NestedMapping{
+    mapping(address => mapping(address => uint256)) public debts;
+
+    function incDebt(address _borrower,uint256 _amount) public {
+        debts[msg.sender][_borrower] += _amount; //borrowerın msg.sender borcu artar
+    }
+    function decDebt(address _borrower,uint256 _amount) public {
+        require(debts[msg.sender][_borrower] >= _amount,"not enough debt");
+        debts[msg.sender][_borrower] -= _amount; //borrowerın msg.sender borcu azalır
+
+    }
+
+    function getDebt(address _borrower) public view returns(uint256){
+        return debts[msg.sender][_borrower];
+    }
+}
